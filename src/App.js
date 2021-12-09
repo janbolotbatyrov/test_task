1da2bf3c06530da735763189bd304e34ff4429f6
Janbolot Batyrov <janbolotbatyrovoff@.com>
2021-12-09 21:46:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const API = "http://localhost:3000/category";
+  const [data, setDate] = useState([]);
+
+  const getData = async () => {
+    await fetch(API)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setDate(data);
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Тестовое задание</h1>
+      <div className="category">
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              {item.name}
+              <ul>
+                {item.categories.map((item) => (
+                  <li>
+                    {item.name}
+                    <ul>
+                      {item.categories.map((item) => (
+                        <li>{item.name}</li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
